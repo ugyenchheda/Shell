3c71520590e05e678371d02db2fea74bcfae1d0e
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-08-25 20:39:33 +0300

    typed text overlayed on to be typed text

--- tutorial.py
+++ tutorial.py
@@ -8,33 +8,31 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+
+    #enumerate will give current element along index from the list
+    for i, char in enumerate(current):
+        stdscr.addstr(0, i, char, curses.color_pair(1))
+
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this app!"
     current_text = []
 
-    while True:
-        key = stdscr.getkey()
+    while True:        
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text)
+        stdscr.refresh()
 
+        key = stdscr.getkey()
+        
         if ord(key) == 27:
             break
-
-        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+        if key in (chr(curses.KEY_BACKSPACE), '\b', '\x7f'):
             if len(current_text) > 0:
                 current_text.pop()
-            else:
-                current_text.append(key)
-
-        current_text.append(key)
-        
-        stdscr.clear()
-        stdscr.addstr( target_text)
-        stdscr.refresh()
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
-
-        
-        stdscr.refresh()
+        else:
+            current_text.append(key)     
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
