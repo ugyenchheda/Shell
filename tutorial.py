9642fec304e8928411df43059284a8db11034390
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-08-25 20:50:26 +0300

    color variation added when type text is correct and incorrect

--- tutorial.py
+++ tutorial.py
@@ -13,7 +13,11 @@ def display_text(stdscr, target, current, wpm=0):
 
     #enumerate will give current element along index from the list
     for i, char in enumerate(current):
-        stdscr.addstr(0, i, char, curses.color_pair(1))
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(0, i, char, color)
 
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this app!"
@@ -36,8 +40,8 @@ def wpm_test(stdscr):
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     
     start_screen(stdscr)
